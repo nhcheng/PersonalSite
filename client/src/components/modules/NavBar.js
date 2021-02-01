2caa42f56d0f9059c03bc0272072ccff99a3442f
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2021-02-01 19:59:38 +0000

    seperating Google to another component

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import GoogleLogin, { GoogleLogout } from "react-google-login";
 import { Link } from "@reach/router";
+import MyGoogle from "./MyGoogle.js";
 
 import "../../utilities.css";
 import "./NavBar.css";
-
-const GOOGLE_CLIENT_ID = "405384603408-sa99spi7lqpr247fsstocelfvtln9nnn.apps.googleusercontent.com";
 
 class NavBar extends Component {
   constructor(props) {
@@ -45,23 +43,9 @@ class NavBar extends Component {
               <Link to="/eca" className="NavBar-link">
                   ECA
               </Link>
-            {this.props.userId ? (
-              <GoogleLogout
-                clientId={GOOGLE_CLIENT_ID}
-                buttonText="Logout"
-                onLogoutSuccess={this.props.handleLogout}
-                onFailure={(err) => console.log(err)}
-                className="NavBar-link NavBar-login"
-              />
-            ) : (
-              <GoogleLogin
-                clientId={GOOGLE_CLIENT_ID}
-                buttonText="Login"
-                onSuccess={this.props.handleLogin}
-                onFailure={(err) => console.log(err)}
-                className="NavBar-link NavBar-login"
-              />
-            )}
+            <MyGoogle handleLogin={this.props.handleLogin}
+            handleLogout={this.props.handleLogout}
+            userId={this.props.userId} />
             </div>
         </nav>
       </>
