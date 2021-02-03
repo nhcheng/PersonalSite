2a541581edcd59db696cad5b786c7e859b39a5a8
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2021-02-03 03:22:41 +0000

    Stage 1 - Home and Bottom Banner

--- client/src/components/modules/BottomBanner.js
+++ client/src/components/modules/BottomBanner.js
@@ -4,6 +4,31 @@ import Institutes from "./Institutes.js";
 
 import "../../utilities.css";
 import "./BottomBanner.css";
+import fbIcon from "../../img/icon/facebookicon.png";
+import githubIcon from "../../img/icon/githubicon.png";
+import linkedinIcon from "../../img/icon/linkedinicon.png";
+import emailIcon from "../../img/icon/emailicon.png";
+
+
+class ContactItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="contact-item">
+        <div className="contact-itemleft"> 
+        <img src={this.props.src}/>
+        </div>
+        <div> {this.props.detail} </div>
+      </div>
+    )
+  }
+}
+
+
 
 class BottomBanner extends Component {
   constructor(props) {
@@ -33,16 +58,30 @@ class BottomBanner extends Component {
                 The webpage is developed with the skeleton code provided by 
                 the <a href="https://weblab.mit.edu/">MIT web.lab team</a>, and templates 
                 by <a href="https://lochungtin.github.io/site/">Timothy Lo</a> and <a href="https://nhcheng.mit.edu/">Thomas Cheng</a>. 
-                The code for this webpage is available in my GitHub.
+                The source code for this webpage is available in my GitHub.
           </div>
         </div>
         <div className="bottombanner-right"> 
-            Email:
+            {/* Email:
             <ul>
                 <li> Imperial: chun.lam18@imperial.ac.uk </li>
                 <li> MIT: chunhei@mit.edu </li>
                 <li> Others: sammuelchlam@gmail.com </li>
-            </ul>
+            </ul> */}
+            <div> 
+              <ContactItem src={fbIcon} 
+                detail={(<a href="https://www.facebook.com/chunhei.lam.1671/">Chun Hei Sammuel Lam</a>)}/>
+              <ContactItem src={githubIcon} 
+                detail={(<a href="https://github.com/Samuel-CHLam">Samuel-CHLam</a>)}/>
+              <ContactItem src={linkedinIcon} 
+                detail={(<> <a href="https://www.linkedin.com/in/ch-sammuel-6b446a190/">CH-Sammuel Lam</a> <span> (Inactive) </span> </>)}/>
+              <ContactItem src={emailIcon} 
+              detail={<>
+                <div> <a href="mailto:chun.lam18@imperial.edu"> chun.lam18@imperial.edu </a> (Imperial) </div>
+                <div> <a href="mailto:chunhei@mit.edu"> chunhei@mit.edu </a> (MIT) </div>
+                <div> <a href="mailto:sammuelchlam@gmail.com"> sammuelchlam@gmail.com </a> (Others) </div>
+              </>}/>
+            </div>
         </div>
       </div>
     );
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,6 +5,7 @@ import "./Home.css";
 import { Link } from "@reach/router";
 import TopBanner from "../modules/TopBanner.js";
 import HomeBackground from "../../img/PersonalBanner(2).png";
+import randomWalk from "../../img/homerandom.png";
 
 class Home extends Component {
   constructor(props) {
@@ -31,25 +32,38 @@ class Home extends Component {
           Credit to Daniel Lo."
         />
         <div className="home-block">
-          <h2> As an applied mathematician...</h2>
+        <div className="home-flexbox">
+          <div className="home-flexleft">
+            <h2> As an applied mathematician...</h2>
+            <p style={{margin: 0}}>
+              I mainly work in two areas of mathematics:
+            </p>
+            <ul>
+              <li> <b> Theory of Statistics: </b> Develop fast algorithms for bayesian computations (e.g.
+              Monte-Carlo); study statistical properties of estimators arisen from machine learning algorithms
+              (e.g. components in PCA); applying statistical techniques to analyse and interpret real-life data. 
+              </li>
+              <li> <b> Dynamical System: </b> Utilise the connection between dynamical systems (e.g.
+              Lorenz system) and stochastic processes to establish quantitative results. I am also
+              drawn to the study of return maps of chaotic systems.
+              </li> 
+            </ul>
+          </div>
+          <div className="home-flexright">
+            <img src={randomWalk}/>
+            <p> &#9654; A trajectory simulated by a chaotic process, which
+              resembles random walk. </p>
+          </div>
+        </div>
+        </div>
+        <div className="home-block">
           <p>
-            I mainly work in theory of statistics - I am intrigued by how different areas of mathematics
-            come together to develop theories of machine learning and bayesian computations. For instance,
-            the Monte-Carlo technique in simulating posterior distributions in a bayesian inference problem
-            is inspired by dynamics of particles in a potential well. I would like to study the statistical 
-            properties of estimators arisen from machine learning algorithms, as well as efficiency of
-            estimations of distributions.
-          </p>
-          <p>
-            In addition, I am interested in stochastic analysis - in particular the connection between
-            deterministic dynamical systems and stochastic differential equations (SDE). 
-            It is a well-known fact that diffusions can be characterised
-            by both partial differential equations (PDE) and SDE, but recent studies suggest 
-            that diffusion might also be characterised by chaotic systems. I would like to explore
-            how this connection might be used to establish better quantitative estimates of
-            the solutions of diffusions. Currently I am also drawn to the study of return maps
-            of chaotic systems.
-          </p>
+            In addition to my research, I am creating resources to enhance undergraduates' experiences
+            in learning computational mathematics and statistics. These include a book on multivariate analysis with
+            demonstration in Julia language, and a repository of numerical methods. I am also a peer
+            tutor at Imperial College helping current freshmen (first-year undergraudates) to adopt
+            university life in a rapidly-changing Covid-19 situation.
+            </p>
           <Link to="/math"> Click here for more details and publications. </Link>
           <p>
             My personal journey in mathematics began when I entered the HKCCCU Logos Academy (a comprehensive
@@ -58,16 +72,9 @@ class Home extends Component {
             College London at the age of 16.
           </p>
           <Link to="/mystory"> Click here to read my stories. </Link>
-        </div>
+          </div>
         <div className="home-block">
-          <h2> Outside of academia... </h2>
-          <p>
-            I am working on creating resources to enhance undergraudates' experiences in learning 
-            computational mathematics and statistics. These include a book on multivariate analysis with
-            demonstration in Julia language, and a repository of numerical methods. I am also a peer
-            tutor at Imperial College helping current freshmen (first-year undergraudates) to adopt
-            university life in a rapidly-changing Covid-19 situation.
-          </p>
+          <h2> Outside academia... </h2>
           <p>
             In addition, I enjoy performing music (mainly by playing piano), building Minecraft architectures, 
             exploring Christianity and developing webpages.
